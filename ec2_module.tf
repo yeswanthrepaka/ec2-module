028838f1ad83f8d78dfe90b48abf8d9682fa01f7
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-08 17:29:52 +0530

    changes done

--- ec2_module.tf
+++ ec2_module.tf
@@ -11,17 +11,3 @@ module "ec2_module" {
         Component = var.component
     }
 }
-
-
-/* module "ec2" {
-    source = "../terraform-aws-instance"
-    project = var.project
-    environment = var.environment
-    ami_id = var.ami_id
-    sg_ids = var.sg_ids
-    instance_type = var.instance_type
-    tags = {
-        Name = "${var.project}-${var.environment}-${var.component}"
-        Component = var.component
-    }
-} */
